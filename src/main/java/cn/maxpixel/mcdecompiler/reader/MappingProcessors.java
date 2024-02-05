a6b252ad96c98fffd4be0c90e2593bd8e5e51f19
XiaoPangxie732 <47449269+XiaoPangxie732@users.noreply.github.com>
2024-02-05 21:17:09 +0800

    Fix tiny v1 bad read

--- src/main/java/cn/maxpixel/mcdecompiler/reader/MappingProcessors.java
+++ src/main/java/cn/maxpixel/mcdecompiler/reader/MappingProcessors.java
@@ -383,6 +383,7 @@ public interface MappingProcessors {
                     }
                 } else error();
             });
+            mappings.left().addAll(classes.values());
             return mappings;
         }
 
