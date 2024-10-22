6c4a0da229970ea629b1179d9d87b6656314e57e
Asbestosstar <ricsch97@gmail.com>
2024-10-22 14:36:47 -0700

    Update DefaultDetectionUnit.java

--- modules/mapping-format-detector/src/main/java/cn/maxpixel/mcdecompiler/mapping/detector/DefaultDetectionUnit.java
+++ modules/mapping-format-detector/src/main/java/cn/maxpixel/mcdecompiler/mapping/detector/DefaultDetectionUnit.java
@@ -17,6 +17,7 @@ public class DefaultDetectionUnit implements DetectionUnit {
     public Optional<MappingFormat<?, ?>> detectHeader(String firstLine) {
         if (firstLine.startsWith("v1")) return Optional.of(MappingFormats.TINY_V1);
         else if (firstLine.startsWith("tiny\t2\t0")) return Optional.of(MappingFormats.TINY_V2);
+        else if (firstLine.contains("¶") || firstLine.contains("\\u00B6")) return Optional.of(MappingFormats.PDME);
         else if (firstLine.startsWith("tsrg2")) return Optional.of(MappingFormats.TSRG_V2);
         return Optional.empty();
     }
@@ -26,6 +27,7 @@ public class DefaultDetectionUnit implements DetectionUnit {
         String s = contents.get(0);
         if (s.startsWith("PK: ") || s.startsWith("CL: ") || s.startsWith("FD: ") || s.startsWith("MD: ")) return Pair.of(MappingFormats.SRG, Percentage.NINETY_NINE);
         else if (s.endsWith(":")) return Pair.of(MappingFormats.PROGUARD, Percentage.NINETY);
+        else if (s.contains("¶") || s.contains("\\u00B6")) return Pair.of(MappingFormats.PDME, Percentage.NINETY);
         s = contents.get(1);
         if (s.startsWith("\t")) return Pair.of(MappingFormats.TSRG_V1, Percentage.FIFTY);
         else return Pair.of(MappingFormats.CSRG, Percentage.FIFTY);
